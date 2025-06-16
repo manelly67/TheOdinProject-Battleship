1713076fd399050b97f76e24b0931775c8c87f8f
manellylopez <manellylopezqqq@gmail.com>
2025-06-16 16:48:13 -0500

    add meta description

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import './style-reset.css';
-import './style-base.css';
-import './style-mobile.css';
-import './style-tablet.css';
-import './style.css';
+import "./style-reset.css";
+import "./style-base.css";
+import "./style-mobile.css";
+import "./style-tablet.css";
+import "./style.css";
+import { screenController } from "./components/screen-controller.js";
 
+const meta = document.createElement("meta");
+meta.setAttribute("name", "description");
+meta.setAttribute("content", "manelly67\'s exercise study project: Battleship within the curriculum The Odin Project");
 
+function component() {
+  const gameContainer = document.createElement("main");
+  gameContainer.setAttribute("id", "container");
+  gameContainer.setAttribute("translate", "no");
+  screenController(gameContainer);
 
-import { screenController } from './components/screen-controller.js';
+  return gameContainer;
+}
 
-function component() { 
-    
-    const gameContainer = document.createElement('main');
-    gameContainer.setAttribute('id','container');
-    gameContainer.setAttribute('translate','no');
-    screenController(gameContainer);
-
-   
-    return gameContainer;
-  }
-  
+document.head.appendChild(meta);
 document.body.appendChild(component());
